2b6ee716665cce0f62c44b637a3ec5c6455784b2
Mike Goeke <pearlman56@hotmail.com>
2024-05-08 21:34:36 -0400

    update SlackAIMessageConverter to convert the Slack Channel file messages into training json files.

--- SlackAIMessageProcessor/SlackAIMessageConverter.cs
+++ SlackAIMessageProcessor/SlackAIMessageConverter.cs
@@ -1,7 +1,13 @@
-﻿namespace SlackAIMessageProcessor
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace SlackAIMessageProcessor
 {
     public class SlackAIMessageConverter
     {
+        private const string TrainingFileName = "TrainingFile.jsonl";
+        private const string ValidationFileName = "ValidationFile.jsonl";
+
         public SlackAIMessageConverter()
         {
             SlackAIMessages = new List<AIMessage>().AsReadOnly();
@@ -19,6 +25,34 @@
             }
 
             SlackAIMessages = slackAIMessages.AsReadOnly();
+        }
+
+        public void CreateTrianingFiles()
+        {
+            var trainingData = new StringBuilder();
+            var validationData = new StringBuilder();
+
+            var trainingSetCount = (int)(SlackAIMessages.Count * 0.8);
+            var validationSetCount = SlackAIMessages.Count - trainingSetCount;
+
+            var trainingSet = SlackAIMessages.Take(trainingSetCount).ToList();
+            var validationSet = SlackAIMessages.Skip(trainingSetCount).Take(validationSetCount).ToList();
+
+            foreach (var message in trainingSet)
+            {
+                trainingData.AppendLine(JsonConvert.SerializeObject(message));
+            }
+
+            foreach (var message in validationSet)
+            {
+                validationData.AppendLine(JsonConvert.SerializeObject(message));
+            }
+
+            File.Delete(TrainingFileName);
+            File.Delete(ValidationFileName);
+
+            File.WriteAllText(TrainingFileName, trainingData.ToString());
+            File.WriteAllText(ValidationFileName, validationData.ToString());
         }
     }
 }
